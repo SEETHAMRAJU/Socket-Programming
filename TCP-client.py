3a453f98303ff97c585b1bceaca920cdee5320ba
Seethamraju Purvaj <seethamraju purvaj>
2018-03-18 22:16:54 +0530

    Comments

--- TCP-client.py
+++ TCP-client.py
@@ -1,27 +1,23 @@
+####         TCP -Client Program for sending 5 numbers ########
+
 import socket
 import random
 port = random.randint(5000,9000)
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-address = ("",8559)
-i =0 
-P = ""
-print "HI"
-while(i<5):
-    P = P + str(random.randint(1,10)) + " "
-    i += 1
-print "P prepared"
-print "P before : " + P
-client.connect(address)
-#while 1:
-i = 0
-    #while(i<5):
-    #    p = random.randint(1,10)
-    #    client.send(str(p))
-    #    i += 1
-print 'Connected'
-client.send(P)
-print "Data Sent"
-data = client.recv(1024)
-print data
-
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)       #Creating a socket
+address = ("172.16.81.152",9272)
+i =0
+client.connect(address)                                         #Connecting to a server of desired choice
+while 1: 
+    i = 0 
+    P = ""          #For sending the given numbers in the form of string
+    while(i<5):                                     
+        P = P + str(random.randint(1,10)) + " "         #Generating Random numbers
+        i += 1
+    print "P before : " + P 
+    i = 0
+    client.send(P)                  #Sending the data
+    print "Data Sent"
+    data = client.recv(1024)        #Recieving data from the server 
+    print data                      #Printing the server
+    break;    
 
